8b46804e1b3c7f44b288ac80c57d5a24fda37bb5
ganesh1172 <ganesh172595@gmail.com>
2023-02-22 16:55:50 +0530

    remove project

--- rest-api-countries/src/components/CountryCard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CountryCard = () => {
-  return <div>CountryCard</div>;
-};
-
-export default CountryCard;
--- rest-api-countries/src/components/CountryLists.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CountryLists = () => {
-  return <div>CountryLists</div>;
-};
-
-export default CountryLists;
