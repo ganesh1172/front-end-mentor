01d5633025446d85dd1624446888e28775d5e65d
ganesh1172 <ganesh172595@gmail.com>
2023-03-08 17:46:10 +0530

    create html structure

--- react-calculator-app/src/App.js
+++ react-calculator-app/src/App.js
@@ -1,9 +1,43 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Calculator</h1>
+    <div className="container">
+      <Header />
+      <div className="result">
+        <h1>3444</h1>
+      </div>
+      <div className="main">
+        <div className="row">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>DEL</button>
+        </div>
+        <div className="row">
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>+</button>
+        </div>
+        <div className="row">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>-</button>
+        </div>
+        <div className="row">
+          <button>.</button>
+          <button>0</button>
+          <button>/</button>
+          <button>x</button>
+        </div>
+        <div className="row">
+          <button>RESET</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ react-calculator-app/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="heading">
+      <h4>calc</h4>
+      <div className="theme">
+        <h5>Theme</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
